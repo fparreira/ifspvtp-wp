0fcea5f2441aaa69cdf7496cb41f6a2f98482e9f
Fernando Parreira <fparreira@gmail.com>
2015-08-26 09:38:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>IFSP - Campus Votuporanga</title>
+
+    <link href="<?php bloginfo("template_url"); ?>/style.css" rel="stylesheet">
+    <link href="<?php bloginfo("template_url"); ?>/font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php bloginfo("template_url"); ?>/assets/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="<?php bloginfo("template_url"); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <!-- header ----------------------------------------->
+    <header style="border: 1px solid red">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <a class="navbar-brand" href="#">
+                        <img src="<?php bloginfo("template_url"); ?>/assets/images/brasil-band.png" class="img-responsive">BRASIL
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="#">Acesso à informação</a>
+                        </li>
+                        <li>
+                            <a href="#">Participe</a>
+                        </li>
+                        <li>
+                            <a href="#">Serviços</a>
+                        </li>
+                        <li>
+                            <a href="#">Legislação</a>
+                        </li>
+                        <li>
+                            <a href="#">Canais</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="identificacao" class="container" style="background: #fe5567">
+            <div class="row">
+                <div class="col-lg-7" style="background: #fe4522">
+                    <img src="<?php bloginfo("template_url"); ?>/assets/images/logo-ifsp-vtp.png" class="img-responsive" alt="IFSP - Campus Votuporanga">
+                </div>
+                <div class="col-lg-5" style="background: #114596">
+                    <img src="<?php bloginfo("template_url"); ?>/assets/images/facebook.png" class="img-responsive" alt="Facebook">
+                    <img src="<?php bloginfo("template_url"); ?>/assets/images/youtube.png" class="img-responsive" alt="youtube">
+                    <form>
+                        <div class="form-group col-xs-4">
+                            <input type="search" class="form-control input-sm" id="buscar" placeholder="Buscar...">
+                            <button type="submit" class="fa fa-search btn btn-xs"></button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        <nav id="navPrincipal" style="background: #4577ef">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menuPrincipal">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="menuPrincipal">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#">Início</a>
+                        </li>
+                        <li>
+                            <a href="#">Insituição</a>
+                        </li>
+                        <li>
+                            <a href="#">Cursos</a>
+                        </li>
+                        <li>
+                            <a href="#">Processo Seletivo</a>
+                        </li>
+                        <li>
+                            <a href="#">Acadêmico</a>
+                        </li>
+                        <li>
+                            <a href="#">Comunicação</a>
+                        </li>
+                        <li>
+                            <a href="#">contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- body ----------------------------------------->
+    <section id="body" style="background: #c3c456">
+        <div class="container" style="border: 1px solid blue">
+            <div class="row">
+
+                <!-- links ---------------------------->
+                <section id="links" class="col-lg-2" style="background: #875421">
+
+                    <img src="<?php bloginfo("template_url"); ?>/assets/images/105anos.png" class="img-responsive">
+
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-cog"></i><span>CERTIFICAÇÃO</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-bank"></i><span>BIBLIOTECA</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-arrow-down"></i><span>MANUAL</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-black-tie"></i><span>CONSELHO</span>
+                    </a>
+
+                    <div class="tituloLinkAreas">
+                        <span>COORDENADORIAS</span>
+                    </div>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-cog"></i><span>CERTIFICAÇÃO</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-bank"></i><span>BIBLIOTECA</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-arrow-down"></i><span>MANUAL</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-black-tie"></i><span>CONSELHO</span>
+                    </a>
+
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-eye"></i><span>PARREIRA</span>
+                    </a>
+
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-eye"></i><span>PARREIRA</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-eye"></i><span>PARREIRA</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-eye"></i><span>PARREIRA</span>
+                    </a>
+                    <a href="#" class="btn linkAreas">
+                        <i class="fa fa-eye"></i><span>PARREIRA</span>
+                    </a>
+
+                </section>
+
+                <!-- conteudo ---------------------------->
+                <section id="conteudo" class="col-lg-7" style="background: #986512">
+
+                    <div id="destaque" class="container-fluid" style="background: #ffe543">
+                        <div class="row">
+                            <div class="col-lg-12" >
+                                <img src="<?php bloginfo("template_url"); ?>/assets/images/Homenagem-do-IFSP-a-todos-os-estudantes.png" class="img-responsive">
+                                <h1>Homenagem do IFSP a todos os estudantes</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="noticias" class="container-fluid" style="background: #e34452">
+                        <div class="row">
+                            <div class="col-lg-12" >
+                                <h2>Processo Seletivo para Vagas Remanescente de Cursos Técnicos</h2>
+                                <span>O intituto Federal de Educação, Ciência e Tecnologia de São Paulo, torna publico
+                                o gabarito das provas do Processo seletivo para preenchimento de vagas remasnescentes
+                                dos Cursos Técnicos<i class="btn bg-info">Leia Mais</i></span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12" >
+                                <h2>Processo Seletivo: Locais das provas</h2>
+                                <span>O intituto Federal de Educação, Ciência e Tecnologia de São Paulo, torna publico
+                                    a lista com os locais das provas<i class="btn bg-info">Leia Mais</i></span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div id="maisNoticias" class="container-fluid">
+                        <h4>Mais notícias</h4>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item">Cras justo odio</a>
+                            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                            <a href="#" class="list-group-item">Morbi leo risus</a>
+                            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                            <a href="#" class="list-group-item">Vestibulum at eros</a>
+                        </div>
+                    </div>
+
+                </section>
+
+                <!-- banners ---------------------------->
+                <section id="banners" class="col-lg-3" style="background: #127466">
+                    area banners
+                </section>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- rodape --------------------------------------------------------->
+    <footer style="border: 1px solid red">
+        rodape
+    </footer>
+
+    <!-- scripts -->
+    <script src="<?php bloginfo("template_url"); ?>/assets/js/jquery-2.1.4.min.js"></script>
+
+    <script src="<?php bloginfo("template_url"); ?>/assets/js/bootstrap.min.js"></script>
+
+
+    <script>
+        $("document").ready(function(){
+            console.log("teste de jquery");
+        });
+    </script>
+
+</body>
+</html>
